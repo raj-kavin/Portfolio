099b2004c2e1feab6ef58a364f1543d0bccb40ea
rajkavin <ragavkavinraj@gmail.com>
2023-06-07 15:44:07 +0530

    backend changes

--- client1/src/App.js
+++ client1/src/App.js
@@ -1,112 +1,97 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import $ from 'jquery';
+
 
 
 function App() {
   
-  $(document).ready(function () {
-    $(window).scroll(function () {
-      // sticky navbar on scroll script
-      if (this.scrollY > 20) {
-        $(".navbar").addClass("sticky");
-      } else {
-        $(".navbar").removeClass("sticky");
-      }
+    $(document).ready(function () {
+      $(window).scroll(function () {
+        // sticky navbar on scroll script
+        if (this.scrollY > 20) {
+          $(".navbar").addClass("sticky");
+        } else {
+          $(".navbar").removeClass("sticky");
+        }
 
-      // scroll-up button show/hide script
-      if (this.scrollY > 500) {
-        $(".scroll-up-btn").addClass("show");
-      } else {
-        $(".scroll-up-btn").removeClass("show");
-      }
-    });
+        // scroll-up button show/hide script
+        if (this.scrollY > 500) {
+          $(".scroll-up-btn").addClass("show");
+        } else {
+          $(".scroll-up-btn").removeClass("show");
+        }
+      });
 
-    // slide-up script
-    $(".scroll-up-btn").click(function () {
-      $("html").animate({ scrollTop: 0 });
-      // removing smooth scroll on slide-up button click
-      $("html").css("scrollBehavior", "auto");
-    });
+      // slide-up script
+      $(".scroll-up-btn").click(function () {
+        $("html").animate({ scrollTop: 0 });
+        // removing smooth scroll on slide-up button click
+        $("html").css("scrollBehavior", "auto");
+      });
 
-    $(".navbar .menu li a").click(function () {
-      // applying again smooth scroll on menu items click
-      $("html").css("scrollBehavior", "smooth");
-    });
+      $(".navbar .menu li a").click(function () {
+        // applying again smooth scroll on menu items click
+        $("html").css("scrollBehavior", "smooth");
+      });
 
-    // toggle menu/navbar script
-    $(".menu-btn").click(function () {
-      $(".navbar .menu").toggleClass("active");
-      $(".menu-btn i").toggleClass("active");
-    });
+      // toggle menu/navbar script
+      $(".menu-btn").click(function () {
+        $(".navbar .menu").toggleClass("active");
+        $(".menu-btn i").toggleClass("active");
+      });
+
+  
+
+    })
+  
 
 
-    // owl carousel script
-    $(".carousel").owlCarousel({
-      margin: 1,
-      loop: true,
-      autoplay: true,
-      autoplayTimeOut: 2000,
-      autoplayHoverPause: true,
-      responsive: {
-        0: {
-          items: 1,
-          nav: false,
-        },
-        600: {
-          items: 2,
-          nav: false,
-        },
-        1000: {
-          items: 3,
-          nav: false,
-        },
-      },
-    });
-  });
+
 
 
   const [data, setData] = useState([]);
-  const texts = ["Developer"];
-  let index = 0;
-  let textIndex = 0;
+  // const texts = ["Developer"];
+  // let index = 0;
+  // let textIndex = 0;
 
-  function typeText() {
-    if (index < texts[textIndex].length) {
-      document.getElementById("typed-text").textContent +=
-        texts[textIndex].charAt(index);
-      index++;
-      setTimeout(typeText, 150);
-    } else {
-      setTimeout(deleteText, 1000);
-    }
-  }
+  // function typeText() {
+  //   if (index < texts[textIndex].length) {
+  //     document.getElementById("typed-text").textContent +=
+  //       texts[textIndex].charAt(index);
+  //     index++;
+  //     setTimeout(typeText, 150);
+  //   } else {
+  //     setTimeout(deleteText, 1000);
+  //   }
+  // }
 
-  function deleteText() {
-    if (index >= 0) {
-      let typedText = document.getElementById("typed-text").textContent;
-      document.getElementById("typed-text").textContent = typedText.slice(
-        0,
-        -1
-      );
-      index--;
-      setTimeout(deleteText, 100);  
-    } else {
-      index = 0;
-      setTimeout(typeText, 500);
-    }
-  }
+  // function deleteText() {
+  //   if (index >= 0) {
+  //     let typedText = document.getElementById("typed-text").textContent;
+  //     document.getElementById("typed-text").textContent = typedText.slice(
+  //       0,
+  //       -1
+  //     );
+  //     index--;
+  //     setTimeout(deleteText, 100);
+  //   } else {
+  //     index = 0;
+  //     setTimeout(typeText, 500);
+  //   }
+  // }
 
-  useEffect(() => {
-    typeText();
-  });
+  // useEffect(() => {
+  //   typeText();
+  // });
 
 
 
   useEffect(() => {
     async function fetchData() {
       const resp = await axios
-        .get('http://localhost:3003/')
+        .get('api/')
         .catch((err) => console.log(err));
       setData(resp.data);
     }
@@ -142,7 +127,7 @@ function App() {
           <div className="home-content">
             <div className="text-1">Hello, my name is</div>
             <div className="text-2">Kavinraj K</div>
-            <div className="text-3">And I'm a <span id='typed-text'></span></div>
+            <div className="text-3">And I'm a <span>Developer</span></div>
             <a href="https://www.linkedin.com/in/kavinraj-k/" target='blank'>Hire me</a>
           </div>
         </div>
@@ -264,7 +249,7 @@ function App() {
       <section className="teams" id="teams">
         <div className="max-width">
           <h2 className="title">My teams</h2>
-          <div className="carousel owl-carousel">
+          <div className="card1">
             <div className="card">
               <div className="box">
                 <img src="images/profile-1.jpg" alt="" />
